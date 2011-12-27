e4b4bfa0dc17fe30903c32780918379c2c8a9838
Reto Buerki <reet@codelabs.ch>
2011-12-27 13:47:37 +0100

    Introduce Socket_Container record
    This record will be used to store the state of a socket (busy,
    non-busy).

--- src/spawn-pool.adb
+++ src/spawn-pool.adb
@@ -1,5 +1,4 @@
 with Ada.Text_IO;
-with Ada.Directories;
 with Ada.Numerics.Discrete_Random;
 with Ada.Containers.Ordered_Maps;
 with Ada.Strings.Unbounded;
@@ -14,20 +13,34 @@ with Types;
 
 package body Spawn.Pool is
 
-   use Ada.Strings.Unbounded;
-
    Ctx : ZMQ.Contexts.Context;
-
-   type Socket_Handle is access ZMQ.Sockets.Socket;
 
    Mngr_Bin  : constant String := "obj/spawn_manager";
    Addr_Base : constant String := "/tmp/spawn_manager-";
 
+
+   use Ada.Strings.Unbounded;
+
+   type Socket_Handle is access ZMQ.Sockets.Socket;
+
+   type Socket_Container is record
+      Address : Unbounded_String;
+      Handle  : Socket_Handle;
+      Busy    : Boolean;
+   end record;
+
    package Socket_Map_Package is new Ada.Containers.Ordered_Maps
      (Key_Type     => Unbounded_String,
-      Element_Type => Socket_Handle);
-   Sockets : Socket_Map_Package.Map;
+      Element_Type => Socket_Container);
+   package SOMP renames Socket_Map_Package;
+   Sockets : SOMP.Map;
    --  Sockets to spawn managers.
+
+   procedure Insert_Socket (S : Socket_Container);
+   --  Insert new socket into store.
+
+   function Get_Socket return Socket_Handle;
+   --  Return non-busy socket from socket store, mark as busy.
 
    subtype Chars is Character range 'a' .. 'z';
    package Random_Chars is new Ada.Numerics.Discrete_Random
@@ -42,9 +55,10 @@ package body Spawn.Pool is
    procedure Execute (Command : String)
    is
       Query : ZMQ.Messages.Message;
+      Sock  : Socket_Handle := Get_Socket;
    begin
       Query.Initialize (Data => Command);
-      Sockets.First_Element.Send (Msg => Query);
+      Sock.Send (Msg => Query);
       Query.Finalize;
 
       declare
@@ -52,7 +66,7 @@ package body Spawn.Pool is
          Data      : Types.Data_Type;
       begin
          Resultset.Initialize;
-         Sockets.First_Element.recv (Msg => Resultset);
+         Sock.recv (Msg => Resultset);
 
          Data := Types.Deserialize (Buffer => Resultset.getData);
          if Data.Success /= True then
@@ -62,6 +76,14 @@ package body Spawn.Pool is
          Resultset.Finalize;
       end;
    end Execute;
+
+   -------------------------------------------------------------------------
+
+   function Get_Socket return Socket_Handle
+   is
+   begin
+      return Sockets.First_Element.Handle;
+   end Get_Socket;
 
    -------------------------------------------------------------------------
 
@@ -94,10 +116,20 @@ package body Spawn.Pool is
          Sock.Initialize (With_Context => Ctx,
                           Kind         => ZMQ.Sockets.REQ);
          Sock.Connect (Address => Addr);
-         Sockets.Insert (Key      => To_Unbounded_String (Addr),
-                         New_Item => Sock);
+         Insert_Socket (S => (Address => To_Unbounded_String (Addr),
+                              Handle  => Sock,
+                              Busy    => False));
       end;
    end Init;
+
+   -------------------------------------------------------------------------
+
+   procedure Insert_Socket (S : Socket_Container)
+   is
+   begin
+      Sockets.Insert (Key      => S.Address,
+                      New_Item => S);
+   end Insert_Socket;
 
    -------------------------------------------------------------------------
 
