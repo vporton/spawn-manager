28766cd590513b7af1fb8ca2926c73a88c7055a0
Victor Porton <porton@narod.ru>
2017-11-15 15:23:33 +0200

    bug fix

--- src/spawn-spawner.adb
+++ src/spawn-spawner.adb
@@ -67,12 +67,17 @@ package body Spawn.Spawner is
       return Descriptor.Pid;
    end Get_Pid;
 
+   -- FIXME: remove
+   procedure perror (s : char_array);
+   pragma Import (C, perror);
+
    procedure Non_Blocking_Spawn
      (Descriptor  : out Process_Descriptor'Class;
       Command     : String)
    is
       Pid : constant Process_Id := fork;
    begin
+      perror("XXX"); --  FIXME: remove
       case Pid is
          when 0 =>
             --  FIXME: Shall we support shell semantics of use plain execvp()?
@@ -85,9 +90,11 @@ package body Spawn.Spawner is
                  execv (Shell2,
                         (0 => Shell2,
                          1 => To_Chars_Ptr (CFlag'Unchecked_Access),
-                         2 => To_Chars_Ptr (Cmd'Unchecked_Access)));
+                         2 => To_Chars_Ptr (Cmd'Unchecked_Access),
+                         3 => Null_Ptr));
                pragma Unreferenced (R);
             begin
+               perror("YYY"); --  FIXME: remove
                raise Command_Failed with "Cannot exec shell"; --  TODO
             end;
          when -1 =>
